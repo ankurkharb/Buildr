8f3c5185dc16bc825e9dce3a48c06089a3cdeb8b
Ankur Kharb <ankurkharb1234@gmail.com>
2026-05-27 15:08:59 +0530

    Prepare Inngest config for Vercel

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -2,6 +2,7 @@ import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
 import {BuildrAgent } from "@/inngest/functions";
 
+export const maxDuration = 300;
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
